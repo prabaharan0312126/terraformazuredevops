6195b1b4c37e9182d5e7483e8e57b6eaa37b220d
prabaharan0312126 <prabaharanmech@gmail.com>
2022-11-19 23:55:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "azuredevops-terraform"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "azuredevops-terraformDB"
   }
 }
